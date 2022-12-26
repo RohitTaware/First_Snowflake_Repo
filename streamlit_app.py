021dd9c82269cd3c1fad47ad01965dee3c60f5e6
RohitTaware <120206289+RohitTaware@users.noreply.github.com>
2022-12-26 23:14:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
-#import streamlit
+import streamlit
 
-#streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My Parents New Healthy Dinner')
 
-#import snowflake.connector
+import snowflake.connector
 
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchall()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchall()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 import streamlit
 import snowflake.connector
